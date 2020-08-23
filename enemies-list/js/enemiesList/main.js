5e7e3946e18b7399726596eb646bfcb6fe3de555
Tasha Lane <designtmg77@gmail.com>
2020-08-23 16:54:29 -0500

    enemies list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -31,7 +31,45 @@ function main() {
   // Make a list of Enemy objects
   //  How would you create a collection of enemy objects in JavaScript
 
-  const enemies = Enemy.find((p) => p.Enemy == "Joshua Flowers")
+  const enemies = [
+    {
+      FirstName: "Joshua",
+      LastName: "Flowers",
+      Offenses: new List < string > [
+        "Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"
+      ],
+      IsReallyHated = true
+    },
+    {
+      FirstName: "Darth",
+      LastName: "Vader",
+      Offenses: new List < string > [
+        "Cut off Luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"
+      ],
+      IsReallyHated = false
+    },
+    {
+      FirstName: "Betty",
+      LastName: "Rudelady",
+      Offenses: new List < string > [
+        "Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"
+      ],
+      IsReallyHated = true
+    },
+    {
+      FirstName: "Leon",
+      LastName: "Peck",
+      Offenses: new List < string > [
+        "Keeps giving me a hotplate"
+      ],
+      IsReallyHated = false
+    }
+  ]
 }
-
 main();
