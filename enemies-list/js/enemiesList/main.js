f3898f0a200b3ed343c7555fb35c1a63d6b29d49
Tasha Lane <designtmg77@gmail.com>
2020-08-23 16:32:33 -0500

    changes

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,6 +1,37 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+
+
 function main() {
   // Put your code here
 
+  const EnemiesList
+  {
+    console.log("My Enemies List!");
+    console.log("----------------");
+    const enemies = GetEnemies();
+
+    for (let myEnemy of enemies);
+    {
+      if (myEnemy.IsReallyHated);
+      {
+        console.log(`$"{myEnemy.FirstName} {myEnemy.LastName} (Really, really dislike!"`)
+      }
+      else
+      {
+        console.log(`$"{myEnemy.FirstName} {myEnemy.LastName}"`);
+      }
+    }
+
+
+  }
+
+  const Enemy = GetEnemies()
+
+  // Make a list of Enemy objects
+  //  How would you create a collection of enemy objects in JavaScript
+
+  const enemies = Enemy.find((p) => p.Enemy == "Joshua Flowers")
 }
 
 main();
--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,32 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+
+
 function main() {
   // Put your code here
+  const names = ["Nashville", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+}
 
+//Print some output
+console.log("All Place Names");
+
+//Iterate over each
+for (let name of names) {
+  console.log(name);
+
+}
+
+//When you don't pass an argument to console.log.
+console.log();
+
+//Filter Array to create a new list
+//place names that start with the word "The"
+
+const theNames = names.filter((n) => n.StartsWith("The")).ToList();
+
+console.log(" 'The' Place Names");
+for (let theName of theNames) {
+  console.log(name);
 }
 
 main();
